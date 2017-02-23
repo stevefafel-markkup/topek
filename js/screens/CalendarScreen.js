6590f172c181a1690fda9f62db9435f81c996f97
davideweaver <dweaver@LT8FG8WP.local>
2017-02-23 12:44:22 -0500

    added calendar control

--- js/screens/CalendarScreen.js
+++ js/screens/CalendarScreen.js
@@ -1,7 +1,23 @@
 import React, { Component } from "react"
 import { StyleSheet, View, Text, Button } from "react-native"
 import { connectprops, PropMap } from "react-redux-propmap"
+import Calendar from "react-native-calendar"
+import moment from "moment"
 import Styles, { Color, Dims } from "../styles"
+
+const customDayHeadings = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+const customMonthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const calendarStyle = {
+    dayHeading: {
+      color: "black",
+      fontSize: 12
+    },
+    weekendHeading: {
+      color: "black",
+      fontSize: 12
+    },
+  }
 
 class Props extends PropMap {
   map(props) {
@@ -16,10 +32,35 @@ export default class CalendarScreen extends Component {
     title: "Calendar"
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDate: moment().format(),
+    }
+  }
+
   render() {
     return (
       <View style={Styles.screen}>
-        
+        <Calendar
+          ref="calendar"
+          customStyle={calendarStyle}
+          eventDates={['2017-02-03', '2017-02-05', '2017-03-28', '2017-07-30']}
+          events={[{date: '2016-07-04', hasEventCircle: {backgroundColor: 'powderblue'}}]}
+          scrollEnabled
+          showControls={false}
+          dayHeadings={customDayHeadings}
+          monthNames={customMonthNames}
+          titleFormat={'MMMM YYYY'}
+          prevButtonText={"Prev"}
+          nextButtonText={"Next"}
+          onDateSelect={(date) => this.setState({ selectedDate: date })}
+          onTouchPrev={(e) => console.log('onTouchPrev: ', e)}
+          onTouchNext={(e) => console.log('onTouchNext: ', e)}
+          onSwipePrev={(e) => console.log('onSwipePrev: ', e)}
+          onSwipeNext={(e) => console.log('onSwipeNext', e)}
+        />
+        <Text>Selected Date: {moment(this.state.selectedDate).format('MMMM DD YYYY')}</Text>
       </View>
     )
   }
