74fcbf59e138bf7fb7a1cda398f02af2f17255ed
davideweaver <dweaver@LT8FG8WP.local>
2017-02-27 13:45:17 -0500

    added field button

---
+++ js/components/FieldButton.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react"
+import { StyleSheet, View, TouchableHighlight, Text } from "react-native"
+import Icon from "react-native-vector-icons/Ionicons"
+import Styles, { Color, Dims } from "../styles"
+
+export default class FieldButton extends Component {
+  render() {
+    const { title, onPress, disabled } = this.props
+
+    let overlayStyle = {
+      backgroundColor: disabled ? "rgba(0, 0, 0, 0.1)" : "rgba(0, 0, 0, 0)"
+    }
+
+    let textStyle = {
+      color: disabled ? "rgba(255, 255, 255, 0.4)" : "rgba(255, 255, 255, 1)"
+    }
+
+    return (<TouchableHighlight 
+              underlayColor={Color.tint} 
+              activeOpacity={0.6} 
+              disabled={disabled}
+              onPress={onPress} 
+              style={styles.button}>
+              <View 
+                style={[styles.overlay, overlayStyle]}>
+                <Text 
+                  style={[styles.buttonText, textStyle]}>
+                  {title}
+                </Text>
+              </View>
+          </TouchableHighlight>)
+  }
+}
+
+FieldButton.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  onPress: React.PropTypes.func.isRequired,
+  disabled: React.PropTypes.bool
+}
+
+FieldButton.defaultProps = {
+  disabled: false
+}
+
+let styles = StyleSheet.create({
+  button: {
+    backgroundColor: Color.tint,
+    padding: 0,
+    marginHorizontal: 10,
+    marginTop: 15,
+    borderRadius: 2
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#fff",
+    fontWeight: "600",
+    fontSize: 16,
+    padding: 10
+  },
+  overlay: {
+  }
+})
--- js/components/index.js
+++ js/components/index.js
@@ -2,10 +2,12 @@ import ErrorHeader from "./ErrorHeader"
 import NavbarButton from "./NavbarButton"
 import TabIcon from "./TabIcon"
 import ToolbarButton from "./ToolbarButton"
+import FieldButton from "./FieldButton"
 
 export {
   ErrorHeader,
   NavbarButton,
   TabIcon,
-  ToolbarButton
+  ToolbarButton,
+  FieldButton
 }
